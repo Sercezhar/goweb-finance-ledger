9745b7d1e5ba89d243053cb92a4ccd9975214a75
Oleksandr Havryliuk <87950995+Sercezhar@users.noreply.github.com>
2023-01-30 00:44:05 +0200

    update BusinessCasesGalleryModal

--- src/components/BusinessCases/BusinessCasesGallery/BusinessCasesGalleryModal/BusinessCasesGalleryModal.jsx
+++ src/components/BusinessCases/BusinessCasesGallery/BusinessCasesGalleryModal/BusinessCasesGalleryModal.jsx
@@ -1,5 +1,5 @@
 import useClickOutside from 'hooks/useClickOutside';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import styles from './BusinessCasesGalleryModal.module.scss';
 
 function BusinessCasesGalleryModal({
@@ -9,6 +9,7 @@ function BusinessCasesGalleryModal({
   nextImage,
   closeModal,
 }) {
+  const [isLoading, setIsLoading] = useState(true);
   const modalRef = useRef(null);
 
   useClickOutside(modalRef, closeModal);
@@ -38,38 +39,52 @@ function BusinessCasesGalleryModal({
   return (
     <div className={styles.wrapper}>
       <div className={styles.imageModal} ref={modalRef}>
-        <img
-          src={require(`../../../../assets/images/cases/${images[imageIndex].url}@2x.jpg`)}
-          alt="people"
-        />
-
-        <div className={styles.details}>
-          <div className={styles.description}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis,
-            sunt.
-          </div>
-          <div className={styles.numerosity}>
-            Image {imageIndex + 1} of {images.length}
-          </div>
+        <div
+          className={styles.loader}
+          style={{ display: isLoading ? 'block' : 'none' }}
+        >
+          <img
+            src={require('../../../../assets/images/gallery/loading.gif')}
+            alt="loader"
+          />
         </div>
 
-        <button
-          className={styles.buttonClose}
-          type="button"
-          onClick={closeModal}
-        ></button>
+        <img
+          style={{ display: isLoading ? 'none' : 'block' }}
+          src={require(`../../../../assets/images/cases/${images[imageIndex].url}@2x.jpg`)}
+          alt="people"
+          onLoad={() => setIsLoading(false)}
+        />
 
-        {imageIndex > 0 ? (
-          <div className={styles.wrapperPrev} onClick={() => prevImage()}>
-            <button className={styles.buttonPrev} type="button"></button>
+        <div style={{ display: isLoading ? 'none' : 'block' }}>
+          <div className={styles.details}>
+            <div className={styles.description}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis,
+              sunt.
+            </div>
+            <div className={styles.numerosity}>
+              Image {imageIndex + 1} of {images.length}
+            </div>
           </div>
-        ) : null}
 
-        {imageIndex < images.length - 1 ? (
-          <div className={styles.wrapperNext} onClick={() => nextImage()}>
-            <button className={styles.buttonNext} type="button"></button>
-          </div>
-        ) : null}
+          <button
+            className={styles.buttonClose}
+            type="button"
+            onClick={closeModal}
+          ></button>
+
+          {imageIndex > 0 ? (
+            <div className={styles.wrapperPrev} onClick={() => prevImage()}>
+              <button className={styles.buttonPrev} type="button"></button>
+            </div>
+          ) : null}
+
+          {imageIndex < images.length - 1 ? (
+            <div className={styles.wrapperNext} onClick={() => nextImage()}>
+              <button className={styles.buttonNext} type="button"></button>
+            </div>
+          ) : null}
+        </div>
       </div>
     </div>
   );
