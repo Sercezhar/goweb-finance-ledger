b63fb032e2612c04f92584bc5c975307337cb956
Oleksandr Havryliuk <87950995+Sercezhar@users.noreply.github.com>
2023-01-29 17:52:57 +0200

    update Navigation.jsx

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,21 +1,44 @@
+import { useMediaQuery } from 'react-responsive';
 import { Link } from 'react-scroll';
 import styles from './Navigation.module.scss';
 
 function Navigation() {
+  const isMobile = useMediaQuery({ query: '(max-width: 767px)' });
+  const scrollOffset = isMobile ? -120 : -70;
+
   return (
     <nav className={styles.navigation}>
       <Link activeClass={styles.active} spy smooth to="home">
         Home
       </Link>
-      <Link activeClass={styles.active} spy smooth to="about">
+      <Link
+        activeClass={styles.active}
+        offset={scrollOffset}
+        spy
+        smooth
+        to="about"
+      >
         About
       </Link>
-      <Link activeClass={styles.active} spy smooth to="cases">
+      <Link
+        activeClass={styles.active}
+        offset={scrollOffset}
+        spy
+        smooth
+        to="cases"
+      >
         Cases
       </Link>
-      <Link activeClass={styles.active} spy smooth to="blog">
+      <Link
+        activeClass={styles.active}
+        offset={scrollOffset}
+        spy
+        smooth
+        to="blog"
+      >
         Blog
       </Link>
+
       <Link activeClass={styles.active} spy smooth to="contact">
         Contact
       </Link>
