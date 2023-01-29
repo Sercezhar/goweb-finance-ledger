daac41c3e637108f796e9d53b8bee5a9783cbc50
Oleksandr Havryliuk <87950995+Sercezhar@users.noreply.github.com>
2023-01-30 01:21:15 +0200

    update BusinessCasesGalleryModal

--- src/components/BusinessCases/BusinessCasesGallery/BusinessCasesGalleryModal/BusinessCasesGalleryModal.jsx
+++ src/components/BusinessCases/BusinessCasesGallery/BusinessCasesGalleryModal/BusinessCasesGalleryModal.jsx
@@ -20,12 +20,12 @@ function BusinessCasesGalleryModal({
         closeModal();
       }
 
-      if (imageIndex < images.length - 1 && event.code === 'ArrowRight') {
-        nextImage();
+      if (imageIndex > 0 && event.code === 'ArrowLeft') {
+        handleOnPrevImage();
       }
 
-      if (imageIndex > 0 && event.code === 'ArrowLeft') {
-        prevImage();
+      if (imageIndex < images.length - 1 && event.code === 'ArrowRight') {
+        handleOnNextImage();
       }
     }
 
@@ -36,55 +36,75 @@ function BusinessCasesGalleryModal({
     };
   });
 
+  function handleOnPrevImage() {
+    setIsLoading(true);
+    prevImage();
+  }
+
+  function handleOnNextImage() {
+    setIsLoading(true);
+    nextImage();
+  }
+
   return (
     <div className={styles.wrapper}>
-      <div className={styles.imageModal} ref={modalRef}>
-        <div
-          className={styles.loader}
-          style={{ display: isLoading ? 'block' : 'none' }}
-        >
-          <img
-            src={require('../../../../assets/images/gallery/loading.gif')}
-            alt="loader"
-          />
-        </div>
-
+      <div
+        className={styles.loader}
+        style={{ display: isLoading ? 'block' : 'none' }}
+      >
         <img
-          style={{ display: isLoading ? 'none' : 'block' }}
+          src={require('../../../../assets/images/gallery/loading.gif')}
+          alt="loader"
+        />
+      </div>
+
+      <div
+        className={styles.imageModal}
+        style={{ display: isLoading ? 'none' : 'block' }}
+        ref={modalRef}
+      >
+        <img
           src={require(`../../../../assets/images/cases/${images[imageIndex].url}@2x.jpg`)}
           alt="people"
           onLoad={() => setIsLoading(false)}
         />
 
-        <div style={{ display: isLoading ? 'none' : 'block' }}>
-          <div className={styles.details}>
-            <div className={styles.description}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis,
-              sunt.
-            </div>
-            <div className={styles.numerosity}>
-              Image {imageIndex + 1} of {images.length}
-            </div>
+        <div className={styles.details}>
+          <div className={styles.description}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis,
+            sunt.
           </div>
-
-          <button
-            className={styles.buttonClose}
-            type="button"
-            onClick={closeModal}
-          ></button>
-
-          {imageIndex > 0 ? (
-            <div className={styles.wrapperPrev} onClick={() => prevImage()}>
-              <button className={styles.buttonPrev} type="button"></button>
-            </div>
-          ) : null}
-
-          {imageIndex < images.length - 1 ? (
-            <div className={styles.wrapperNext} onClick={() => nextImage()}>
-              <button className={styles.buttonNext} type="button"></button>
-            </div>
-          ) : null}
+          <div className={styles.numerosity}>
+            Image {imageIndex + 1} of {images.length}
+          </div>
         </div>
+
+        <button
+          className={styles.buttonClose}
+          type="button"
+          onClick={closeModal}
+        ></button>
+
+        {imageIndex > 0 ? (
+          <div
+            className={styles.wrapperPrev}
+            onClick={() => {
+              handleOnPrevImage(true);
+              prevImage();
+            }}
+          >
+            <button className={styles.buttonPrev} type="button"></button>
+          </div>
+        ) : null}
+
+        {imageIndex < images.length - 1 ? (
+          <div
+            className={styles.wrapperNext}
+            onClick={() => handleOnNextImage()}
+          >
+            <button className={styles.buttonNext} type="button"></button>
+          </div>
+        ) : null}
       </div>
     </div>
   );
