1b6566393c1538c3c529f3d653c0b328555a3147
Oleksandr Havryliuk <87950995+Sercezhar@users.noreply.github.com>
2023-01-31 23:21:54 +0200

    update ResponsiveImage.jsx

--- src/components/ResponsiveImage.jsx
+++ src/components/ResponsiveImage.jsx
@@ -4,13 +4,13 @@ function ResponsiveImage({ folder = 'home', imgName = 'blog', alt = 'image' }) {
       <source
         srcSet={`${require(`./../assets/images/${folder}/${imgName}.webp`)} 1x,
             ${require(`./../assets/images/${folder}/${imgName}@2x.webp`)} 2x`}
-        media="(max-width: 767px)"
+        type="image/webp"
       />
       <source
         srcSet={`${require(`./../assets/images/${folder}/${imgName}.jpg`)} 1x,
             ${require(`./../assets/images/${folder}/${imgName}@2x.jpg`)} 2x`}
-        media="(min-width: 768px)"
       />
+
       <img
         src={require(`./../assets/images/${folder}/${imgName}.jpg`)}
         alt={alt}
